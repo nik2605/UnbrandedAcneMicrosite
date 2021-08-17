69c332a54544cc16fff0a92050c994209e7025d0
Qiao Wang <qwang@patientcaresolutions.com>
2021-08-17 12:26:55 -0400

    add Content-Security-Policy

--- Antibody.UnbrandedAcneMicrosite/Startup.cs
+++ Antibody.UnbrandedAcneMicrosite/Startup.cs
@@ -43,8 +43,7 @@ namespace Antibody.UnbrandedAcneMicrosite
                 context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
                 context.Response.Headers.Add("Referrer-Policy", "no-referrer");
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                //context.Response.Headers.Add("Content-Security-Policy",
-                //"default-src 'self' style-src 'self' 'unsafe-inline';");
+                context.Response.Headers.Add("Content-Security-Policy", "style-src 'self' 'unsafe-inline';");
 
                 await next();
             });
